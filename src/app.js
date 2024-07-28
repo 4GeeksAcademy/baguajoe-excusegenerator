faf61e6b6c8946aec1feb048cb0ff53a7a9afdc8
baguajoe <baguajoe@gmail.com>
2024-07-28 00:17:25 +0000

    joe's excuses

--- src/app.js
+++ src/app.js
@@ -1,11 +1,31 @@
 /* eslint-disable */
 import "bootstrap";
 import "./style.css";
+import "./index.html";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+window.onload = () => {
+  document.querySelector("#button").addEventListener("click", () => {
+    document.querySelector("#excuse").innerHTML = generateExcuse();
+  });
+};
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+let generateExcuse = () => {
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  function random(array) {
+    return array[Math.floor(Math.random() * array.length)];
+  }
+
+  return (
+    random(who) + " " + random(action) + " " + random(what) + " " + random(when)
+  );
 };
